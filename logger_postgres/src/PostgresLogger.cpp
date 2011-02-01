66a33436c4b66c365d68685ae01b1ec80ad4aeb4
Ferdinand Tomale <beowulf1416@gmail.com>
2011-02-01 23:49:40 +0800

    initial code to connect to postgres database

--- logger_postgres/src/PostgresLogger.cpp
+++ logger_postgres/src/PostgresLogger.cpp
@@ -5,11 +5,26 @@
  *      Author: ferd
  */
 
+#include <LoggerException.h>
+#include <string>
+
 #include "PostgresLogger.h"
 
-PostgresLogger::PostgresLogger() {
-	// TODO Auto-generated constructor stub
-
+PostgresLogger::PostgresLogger(const std::string hostname, const std::string port,
+		const std::string options, const std::string tty, const std::string dbname,
+		const std::string username, const std::string password) {
+	_cn = PQsetdbLogin(
+			hostname.c_str(),
+			port.c_str(),
+			options.c_str(),
+			tty.c_str(),
+			dbname.c_str(),
+			username.c_str(),
+			password.c_str()
+		);
+	if(PQstatus(_cn) == CONNECTION_BAD){
+		throw new LoggerException(LOG_INIT,"Unable to connect to the database");
+	}
 }
 
 PostgresLogger::~PostgresLogger() {
--- logger_postgres/src/PostgresLogger.h
+++ logger_postgres/src/PostgresLogger.h
@@ -8,11 +8,18 @@
 #ifndef POSTGRESLOGGER_H_
 #define POSTGRESLOGGER_H_
 
+#include <string>
+#include <postgresql/libpq-fe.h>
+#include <LoggerException.h>
 #include <ILogger.h>
 
 class PostgresLogger: public ILogger {
+private:
+	PGconn *_cn;
 public:
-	PostgresLogger();
+	PostgresLogger(const std::string hostname, const std::string port,
+			const std::string options, const std::string tty, const std::string dbname,
+			const std::string username, const std::string password);
 	virtual ~PostgresLogger();
 };
 
